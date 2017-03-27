9349689968c788e4ea9c69745e36cf96962eb3c8
Teddy Ort <teddy@mit.edu>
2017-03-27 16:13:15 -0400

    Remove setupParam function

--- ROS Node.py
+++ ROS Node.py
@@ -13,11 +13,6 @@ class $NodeName(object):
 
         rospy.loginfo("[%s] has started", self.node_name)
 
-    def setupParam(self,param_name,default_value):
-        value = rospy.get_param(param_name,default_value)
-        rospy.set_param(param_name,value) #Write to parameter server for transparancy
-        rospy.loginfo("[%s] %s = %s " %(self.node_name,param_name,value))
-        return value
 
 if __name__ == '__main__':
     rospy.init_node('$node_name', anonymous=False)
